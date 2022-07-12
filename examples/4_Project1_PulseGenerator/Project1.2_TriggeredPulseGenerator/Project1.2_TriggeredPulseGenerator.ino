9e538ea69332abb6063ecaaf2af9032c0cdf23fb
Kravitz Lab <alexxai@wustl.edu>
2022-07-12 16:43:46 -0500

    updated Examples

--- examples/4_Project1_PulseGenerator/Project1.2_TriggeredPulseGenerator/Project1.2_TriggeredPulseGenerator.ino
+++ examples/4_Project1_PulseGenerator/Project1.2_TriggeredPulseGenerator/Project1.2_TriggeredPulseGenerator.ino
@@ -1,6 +1,7 @@
 /*
    Day 1 Project - build a simple pulse generator for controlling optogenetic stimulation!
-  
+   Blink a pin at a set frequency whenever button 2 is pressed
+
    Lex Kravitz and Kevin Chavez
    alexxai@wustl.edu
    June 2022
@@ -10,28 +11,27 @@ int freq = 5;                                                   // This is our p
 
 void setup() {
   pinMode(13, OUTPUT);                                          // Set pin 13 (the LED) to be an output
+ ///////////////////////////////////////////////////////
+  //Task 1: Set up  pin 2 as an input
+  ///////////////////////////////////////////////////////
+
+
 }
 
 void loop() {
+
+ ///////////////////////////////////////////////////////
+  //Task 2: Use a "while loop" to only pulse when a button on pin 2 is pressed
+  ///////////////////////////////////////////////////////
+  
   //Pulse pin D13 at the set frequency
   digitalWrite (13, HIGH);                                      // Light up LED on D13
   delay(1000 / freq / 2);                                       // Delay for light ON period
   digitalWrite (13, LOW);                                       // Turn off LED on D13
   delay(1000 / freq / 2);                                       // Delay for light OFF period
 
+ ///////////////////////////////////////////////////////
+  //Task 3: Bring your code in from Project1.1 to update the frequency
   ///////////////////////////////////////////////////////
-  //Task 1: Write your code below here to update the value of "freq" using the joystick.  If it's updating too fast, how can you slow it down? 
-  ///////////////////////////////////////////////////////
-  Serial.println(freq);                                         //Send pulsing frequency to the Serial Monitor (good for debugging)
-
-
-
-  
-  ///////////////////////////////////////////////////////
-  //Task 2: When the pulse goes below 0, the program will crash - how can you stop this from happening?
-  ///////////////////////////////////////////////////////
-
-
-
 
 }
