e094546038c254e0771dd977ab4cfc66aee66e84
Cammilr <105751232+Cammilr@users.noreply.github.com>
2022-07-21 12:02:54 -0400

    Update 1.1_Blink.ino

--- examples/1_Outputs/1.1_Blink/1.1_Blink.ino
+++ examples/1_Outputs/1.1_Blink/1.1_Blink.ino
@@ -1,14 +1,18 @@
 /*
   Re-mix of the famous Blink sketch
+  
+  The PyGamer has an on-board LED you can control. This red LED is connected to pin 13, 
+  but you may use LED_BUILTIN. LED_BUILTIN is set to the correct LED pin independent 
+  of which board is used.
 
-  modified 11 Jul 2022
-  by Lex Kravitz
   modified 8 May 2014
   by Scott Fitzgerald
   modified 2 Sep 2016
   by Arturo Guadalupi
   modified 8 Sep 2016
   by Colby Newman
+  modified 11 Jul 2022
+  by Lex Kravitz
 
   This example code is in the public domain.
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
