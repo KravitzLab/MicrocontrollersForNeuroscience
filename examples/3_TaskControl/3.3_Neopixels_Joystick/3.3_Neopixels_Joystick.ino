3a4ea45d3bec2f4f240a55414f58206c0ec80786
Kravitz Lab <alexxai@wustl.edu>
2022-07-24 17:16:19 -0500

    Update 3.3_Neopixels_Joystick.ino

--- examples/3_TaskControl/3.3_Neopixels_Joystick/3.3_Neopixels_Joystick.ino
+++ examples/3_TaskControl/3.3_Neopixels_Joystick/3.3_Neopixels_Joystick.ino
@@ -1,58 +1,50 @@
 /*
-  Example 3.3: Joystick and NeoPixels
-  
-  This code uses the PyGamer's built-in joystick to read X and Y-values
-  to turn on specific NeoPixels. Based on the joystick's position, one
-  or two individual NeoPixels will turn on.
-  
+  Example 3.3: Controlling the NeoPixels with the Joystick
+
+  This code read the X position from the PyGamer's built-in joystick
+  to control which NeoPixels are lit. 
+
   created Jun 2022
   by Cammi Rood & Kevin Chavez Lopez
 */
 
-int JoystickX = A11;
-int pix = 8;
-int numPix = 5;
+#include <Adafruit_NeoPixel.h>                                    // We need to include the Adafruit_NeoPixel library
+#define PIN 8                                                     // The Neopixels are on pin 8 (on different boards this might be different)
+#define NUMPIXELS 5                                               // We have 5 Neopixels
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);   // Define the "pixels" object
 
-int delayVal = 100;
-
-#include <Adafruit_NeoPixel.h>
-Adafruit_NeoPixel pixels(numPix, pix, NEO_GRB + NEO_KHZ800);
+#define JoystickX A11                                             // Assign the variable label JoystickX to pin A11  
 
 void setup() {
-  // put your setup code here, to run once:
-  pixels.begin();
-  pinMode(JoystickX, INPUT);
+  pixels.begin();                                                 // Initialize "pixels" object
+  pinMode(JoystickX, INPUT);                                      // Set JoystickX as an input
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int valX = analogRead(JoystickX);
-  
-  //The meaning of the digits in the parenthesis below:
-  //First digit represents the pixel# from 0 to 4.
-  //The following digits change the color values in a R-G-B scale. Min=0 Max=255
-  pixels.clear();
-  pixels.setPixelColor(2, 10, 10, 10);  
-  if (valX < 200) {
-    pixels.setPixelColor(0, 10, 10, 10);
+  int valX = analogRead(JoystickX);                               // Read JoystickX position 
+  pixels.clear();                                                 // Clear all pixels
+  pixels.setPixelColor(2, 5, 5, 5);                               // Set the 3rd pixel (index = 2) to the RGB color (5, 5, 5) - a dim white
+  if (valX < 25) {                                                // If Joystick is all the way to the left...
+    pixels.setPixelColor(0, 5, 5, 5);                             // Light up left-most pixel in dim white (5, 5, 5)
   }
-  if (valX < 400) {
-    pixels.setPixelColor(1, 10, 10, 10);
+  if (valX < 200) {                                               // If Joystick is a little to the left...
+    pixels.setPixelColor(1, 5, 5, 5);                             // Light up 2nd pixel in dim white (5, 5, 5)
   }
-  if (valX > 600) {
-    pixels.setPixelColor(3, 10, 10, 10);
+  if (valX > 775) {                                               // If Joystick is a little to the right...
+    pixels.setPixelColor(3, 5, 5, 5);                             // Light up 4th pixel in dim white (5, 5, 5)
   }
-  if (valX > 800) {
-    pixels.setPixelColor(4, 10, 10, 10);
+  if (valX > 1000) {                                              // If Joystick is all the way to the right...
+    pixels.setPixelColor(4, 5, 5, 5);                             // Light up 5th pixel in dim white (5, 5, 5)
   }
-  pixels.show();
+  pixels.show();                                                  // show pixels (don't forget this line when using NeoPixels!)
 }
 
 /*
 
-Activities:
-1) Make the left two Neopixels light up in red, and the right two light up in blue when activated
-2) Change *one character* in the above code to change the Neopixels with the Y instead of X position
-3) (Advanced) Modify this code to control the NeoPixels with the accelerometer instead of the light meter
+  Activities:
+  1) Make the left two Neopixels light up in red, and the right two light up in blue when activated
+  2) Change *one character* in the above code to change the Neopixels with the Y instead of X position
+  3) Change a different *single character* in the above code to light the Neopixels with the light sensor 
+  3) (Advanced) Modify this code to control the NeoPixels with the accelerometer instead of the joystick
 
 */
