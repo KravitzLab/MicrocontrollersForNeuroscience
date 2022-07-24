2beb517e684964deb4841c5fecd9c55b1cf0881b
Kravitz Lab <alexxai@wustl.edu>
2022-07-24 15:01:41 -0500

    Update 2.2_ReadLightSensor.ino

--- examples/2_Inputs/2.2_ReadLightSensor/2.2_ReadLightSensor.ino
+++ examples/2_Inputs/2.2_ReadLightSensor/2.2_ReadLightSensor.ino
@@ -11,24 +11,24 @@
   
 */
 
-int sensorPin = A7;                                   // The lightsensor is on pin A7
-int delayVal = 200;                                   // Set our sampling period to 200ms
+#define lightSensor = A7                          // The lightsensor is on pin A7
+int delayVal = 200;                               // Set our sampling period to 200ms
 
 void setup() {
-  Serial.begin(9600);                                 // This will allow us to write data to the computer using the Serial port
+  Serial.begin(9600);                             // This will allow us to write data to the computer using the Serial port
 }
 
 void loop() {
-  int sensorVal = analogRead(sensorPin);              // analogRead from the light sensor, and save the value as "sensorVal"
-  Serial.print ("sensorVal = ");                      // Print to the serial port (open the Serial Monitor in the Arduino IDE to view this readout)
-  Serial.println (sensorVal);                         // Print to the serial port
-  delay (delayVal);                                   // Delay until next sample
+  int light = analogRead(lightSensor);            // analogRead from the light sensor, and save the value as "sensorVal"
+  Serial.print ("Light level = ");                // Print to the serial port (open the Serial Monitor in the Arduino IDE to view this readout)
+  Serial.println (light);                         // Print to the serial port
+  delay (delayVal);                               // Delay until next sample
 }
 
 /*
 
 Activities:
-1. What is the brightest/highest light value?
-2. What is the dimmest/lowest light value?
+1. What is the highest light value the sensor can detect? (try using your cell phone flashlight)
+2. What is the lowest light value the sensor can detect? (cover it with your finger)
 
 */
