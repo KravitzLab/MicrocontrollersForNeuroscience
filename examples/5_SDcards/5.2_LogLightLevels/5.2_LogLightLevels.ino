5ba82f2a3a7f7af33d96805703ad67ccce343012
Kravitz Lab <alexxai@wustl.edu>
2022-07-24 17:58:58 -0500

    Update 5.2_LogLightLevels.ino

--- examples/5_SDcards/5.2_LogLightLevels/5.2_LogLightLevels.ino
+++ examples/5_SDcards/5.2_LogLightLevels/5.2_LogLightLevels.ino
@@ -11,15 +11,16 @@ by Lex Kravitz
 #include <SPI.h>                                       //Include SPI library
 #include <SD.h>                                        //Include SD library
 const int chipSelect = 4;                              //Set "Chip Select" pin for SD card on SPI bus
+#define lightSensor A7                                 //Assign the variable lightSensor to pin A7
 
 void setup() {
   SD.begin(chipSelect);                                //Initialize SD card
-  pinMode(A7, INPUT);
+  pinMode(lightSensor, INPUT);                                  //Set lightSensor as an input
 }
 
 void loop() {
   File dataFile = SD.open("light.csv", FILE_WRITE);    //open file (if the filename doesn't exist this will create a new one)
-  int light = analogRead(A7);                          //read the light level from A7
+  int light = analogRead(lightSensor);                 //read the light level from A7
   dataFile.println(light);                             //write the X position of the joystick to the file and move to the next line
   dataFile.close();                                    //close file (data won't be written on card until file is closed)
   delay(100);                                          //delay 100ms (this results in a 10Hz sampling rate of the light level)
@@ -28,7 +29,8 @@ void loop() {
 /*
 
 Activities:
-1) If the light-level > 600, print "Maximum light" instead of printing the number in the log file
-2) 
+1) If the light-level is >600, print "Bright light" instead of printing the number in the log file
+2) If the light-level is <100, print "Low light" instead of printing the actual reading in the log file
+3) (Advanced) Have the program write just 10 light sensor samples and stop logging.
 
 */
