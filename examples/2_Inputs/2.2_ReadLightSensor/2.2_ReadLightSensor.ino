577dfb7f822e9821c1e68c352e3db408d6a2c53d
Kravitz Lab <alexxai@wustl.edu>
2022-07-24 15:44:45 -0500

    Update 2.2_ReadLightSensor.ino

--- examples/2_Inputs/2.2_ReadLightSensor/2.2_ReadLightSensor.ino
+++ examples/2_Inputs/2.2_ReadLightSensor/2.2_ReadLightSensor.ino
@@ -2,7 +2,7 @@
   Example 2.2: Read from the light sensor
   
   This code demonstrates the use of the built-in light sensor on the PyGamer.
-  It reads from the analog sensor and displays the light value (0-1023) on the
+  It reads from the analog sensor and displays the relative light level on the
   serial monitor.
   
   created Jun 2022
@@ -19,7 +19,7 @@ void setup() {
 }
 
 void loop() {
-  int light = analogRead(lightSensor);            // analogRead from the light sensor, and save the value as "sensorVal"
+  int light = analogRead(lightSensor);            // analogRead from the light sensor, and save the value as "light"
   Serial.print ("Light level = ");                // Print to the serial port (open the Serial Monitor in the Arduino IDE to view this readout)
   Serial.println (light);                         // Print to the serial port
   delay (samplingPeriod);                         // Delay until next sample
@@ -28,7 +28,9 @@ void loop() {
 /*
 
 Activities:
-1. What is the highest light value the sensor can detect? (try using your cell phone flashlight)
-2. What is the lowest light value the sensor can detect? (cover it with your finger)
+1. (More practice with variables) Change the variable name of lightSensor to lightLevel
+2. Change the sampling period to 2s, note the difference in sampling rate on the Serial Monitor
+3. What is the highest light value the sensor can detect? (try using your cell phone flashlight)
+4. What is the lowest light value the sensor can detect? (cover it with your finger)
 
 */
