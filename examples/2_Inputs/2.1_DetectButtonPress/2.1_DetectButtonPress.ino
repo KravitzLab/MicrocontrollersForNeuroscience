2e1602313bcbd96ffce07c16d0ac7d2447bb8e9f
Kravitz Lab <alexxai@wustl.edu>
2022-07-24 15:40:18 -0500

    Update 2.1_DetectButtonPress.ino

--- examples/2_Inputs/2.1_DetectButtonPress/2.1_DetectButtonPress.ino
+++ examples/2_Inputs/2.1_DetectButtonPress/2.1_DetectButtonPress.ino
@@ -12,22 +12,22 @@
   by Kevin Chávez López
 */
 
-#define button1 2                                     // let's call digital pin 2 "button1"
-#define button2 3                                     // let's call digital pin 3 "button2"
+#define Button1 2                                     // let's call digital pin 2 "button1"
+#define Button2 3                                     // let's call digital pin 3 "button2"
 int samplingPeriod = 200;                             // Set the sampling period for buttong presses (in ms)
 
 void setup() {
   Serial.begin(9600);                                 // Start the serial connection to allow communication with the computer using the Serial port
-  pinMode(button1, INPUT);                            // Set button1 as an input
-  pinMode(button2, INPUT);                            // Set button2 as an input
+  pinMode(Button1, INPUT);                            // Set button1 as an input
+  pinMode(Button2, INPUT);                            // Set button2 as an input
 }
 
 void loop() {
-  if (digitalRead(button1) == LOW) {                  // If button1 has been pressed...
+  if (digitalRead(Button1) == LOW) {                  // If button1 has been pressed...
     Serial.println("Button1 Pressed");                // Print "Button1 Pressed"
     delay(samplingPeriod);                            // A short delay helps prevent multiple triggers
   }
-  if (digitalRead(button2) == LOW) {                  // If button2 has been pressed...
+  if (digitalRead(Button2) == LOW) {                  // If button2 has been pressed...
     Serial.println("Button2 Pressed");                // Print "Button2 Pressed"
     delay(samplingPeriod);                            // A short delay helps prevent multiple triggers
   }
@@ -36,8 +36,10 @@ void loop() {
 /*
 
 Activities:
-1. Change the code to report when the buttons are released (and not pressed) to the Serial monitor
-2. Now report both press and release to the Serial monitor
-3. Instead of writing to the Serial monitor, light up the red LED on pin 13 when button1 is pressed 
+1. Editing only two characters, change which button causes the code to print "Button1 Pressed", and vice versa
+2. Change the names of the buttons to "ButtonA" and "ButtonB" throughout the code 
+3. Change the code to report to the Serial monitor when the buttons are released instead of pressed
+4. Now report both press and release events to the Serial monitor
+5. Instead of writing to the Serial monitor, light up the red LED on pin 13 when button1 is pressed 
 
 */
