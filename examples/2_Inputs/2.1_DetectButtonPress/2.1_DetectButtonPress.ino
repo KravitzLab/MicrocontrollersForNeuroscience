cf1c7518e15641f11b84b25e1c40bad2af95cc98
Kravitz Lab <alexxai@wustl.edu>
2022-07-24 14:52:50 -0500

    Update 2.1_DetectButtonPress.ino

--- examples/2_Inputs/2.1_DetectButtonPress/2.1_DetectButtonPress.ino
+++ examples/2_Inputs/2.1_DetectButtonPress/2.1_DetectButtonPress.ino
@@ -1,20 +1,23 @@
 ﻿/*
   Example 2.1 Button Press
   
-  This code uses externally wired buttons to demonstrate the use 
-  of buttons with microcontrollers. The serial monitor produces  
-  feedback via "if" statements.
+  This code uses externally wired buttons to demonstrate how we can detect button presses. 
+  We will use these buttons: https://www.adafruit.com/product/4431
+  Start by plugging two buttons into the ports on the bottom of the PyGamer using 3 pin STEMMA wires.
+  
+  We will also learn a new concept in this example, which is using the Serial monitor 
+  to provide feedback from the microcontroller to the user (you!) via your computer screen.
   
   created Jun 2022
   by Kevin Chávez López
 */
 
-int button1 = 2;                                      // button1 will be plugged into digital pin 2
-int button2 = 3;                                      // button2 will be plugged into digital pin 3
-int delayVal = 200;                                   // Set the sampling interval
+#define button1 2                                     // lets call digital pin 2 "button1"
+#define button2 3                                     // lets call digital pin 3 "button2"
+int delayVal = 200;                                   // Set the delay between button sampling
 
 void setup() {
-  Serial.begin(9600);                                 // This will allow us to write data to the computer using the Serial port
+  Serial.begin(9600);                                 // Start the serial connection to allow communication with the computer using the Serial port
   pinMode(button1, INPUT);                            // Set button1 as an input
   pinMode(button2, INPUT);                            // Set button2 as an input
 }
@@ -24,11 +27,11 @@ void loop() {
   int button2State = digitalRead(button2);            // Read the state of button2 and store it as "button2State"
   if (button1State == LOW) {                          // If button1 has been pressed...
     Serial.println("Button1 Pressed");                // Print "Button1 Pressed"
-    delay(delayVal);                                  // A short delay helps slow down multiple triggers
+    delay(delayVal);                                  // A short delay helps prevent multiple triggers
   }
   if (button2State == LOW) {                          // If button2 has been pressed...
     Serial.println("Button2 Pressed");                // Print "Button2 Pressed"
-    delay(delayVal);                                  // A short delay helps slow down multiple triggers
+    delay(delayVal);                                  // A short delay helps prevent multiple triggers
   }
 
 /*
