5b971235cca158a89dbb677225087564b3704d55
Kravitz Lab <alexxai@wustl.edu>
2022-07-25 10:41:27 -0500

    Update 2.1_DetectButtonPress.ino

--- examples/2_Inputs/2.1_DetectButtonPress/2.1_DetectButtonPress.ino
+++ examples/2_Inputs/2.1_DetectButtonPress/2.1_DetectButtonPress.ino
@@ -1,41 +1,45 @@
 /*
   Example 2.1 Button Press
-
-  This code uses externally wired buttons to demonstrate the use
-  of buttons with microcontrollers. The serial monitor produces
-  feedback via "if" statements.
-
+  
+  This code uses externally wired buttons to demonstrate how we can detect button presses. 
+  We will use these buttons: https://www.adafruit.com/product/4431
+  Start by plugging two buttons into the ports on the bottom of the PyGamer using 3 pin STEMMA wires.
+  
+  We will also learn a new concept in this example, which is using the Serial monitor 
+  to provide feedback from the microcontroller to the user (you!) via your computer screen.
+  
   created Jun 2022
   by Kevin Chávez López
 */
 
-int button1 = 2;                                      // button1 will be plugged into digital pin 2
-int button2 = 3;                                      // button2 will be plugged into digital pin 3
-int delayVal = 200;                                   // Set the sampling interval
+#define Button1 2                                     // let's call digital pin 2 "button1"
+#define Button2 3                                     // let's call digital pin 3 "button2"
+int samplingPeriod = 200;                             // Set the sampling period for buttong presses (in ms)
 
 void setup() {
-  Serial.begin(9600);                                 // This will allow us to write data to the computer using the Serial port
-  pinMode(button1, INPUT);                            // Set button1 as an input
-  pinMode(button2, INPUT);                            // Set button2 as an input
+  Serial.begin(9600);                                 // Start the serial connection to allow communication with the computer using the Serial port
+  pinMode(Button1, INPUT);                            // Set button1 as an input
+  pinMode(Button2, INPUT);                            // Set button2 as an input
 }
 
 void loop() {
-  int button1State = digitalRead(button1);            // Read the state of button1 and store it as "button1State"
-  int button2State = digitalRead(button2);            // Read the state of button2 and store it as "button2State"
-  if (button1State == LOW) {                          // If button1 has been pressed...
+  if (digitalRead(Button1) == LOW) {                  // If button1 has been pressed...
     Serial.println("Button1 Pressed");                // Print "Button1 Pressed"
-    delay(delayVal);                                  // A short delay helps slow down multiple triggers
+    delay(samplingPeriod);                            // A short delay helps prevent multiple triggers
   }
-  if (button2State == LOW) {                          // If button2 has been pressed...
+  if (digitalRead(Button2) == LOW) {                  // If button2 has been pressed...
     Serial.println("Button2 Pressed");                // Print "Button2 Pressed"
-    delay(delayVal);                                  // A short delay helps slow down multiple triggers
+    delay(samplingPeriod);                            // A short delay helps prevent multiple triggers
   }
 }
 
 /*
 
-  Activities:
-  1. Change the code to detect button release as well as button press
-  2. Change the code to detect button state changes.
+Activities:
+1. Editing only two characters, change which button causes the code to print "Button1 Pressed", and vice versa
+2. Change the names of the buttons to "ButtonA" and "ButtonB" throughout the code 
+3. Change the code to report to the Serial monitor when the buttons are released instead of pressed
+4. Now report both press and release events to the Serial monitor
+5. Instead of writing to the Serial monitor, light up the red LED on pin 13 when button1 is pressed 
 
 */
