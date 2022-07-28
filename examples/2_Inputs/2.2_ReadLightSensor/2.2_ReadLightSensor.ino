8305176e05a1a58b09417128f098ad45ad66e3f7
Cammilr <105751232+Cammilr@users.noreply.github.com>
2022-07-28 11:38:18 -0400

    Update 2.2_ReadLightSensor.ino

--- examples/2_Inputs/2.2_ReadLightSensor/2.2_ReadLightSensor.ino
+++ examples/2_Inputs/2.2_ReadLightSensor/2.2_ReadLightSensor.ino
@@ -11,7 +11,7 @@
 */
 
 #define lightSensor A7                            // The lightsensor is on pin A7
-int samplingPeriod = 100;                         // Set our sampling period to 100ms
+int delayVal = 100;                         // Set our sampling period to 100ms
 
 void setup() {
   Serial.begin(9600);                             // This will allow us to write data to the computer using the Serial port
@@ -21,16 +21,15 @@ void loop() {
   int light = analogRead(lightSensor);            // analogRead from the light sensor, and save the value as "light"
   Serial.print ("Light:");                        // Print to the serial port (open the Serial Monitor in the Arduino IDE to view this readout)
   Serial.println (light);                         // Print to the serial port
-  delay (samplingPeriod);                         // Delay until next sample
+  delay (delayVal);                         // Delay until next sample
 }
 
 /*
 
   Activities:
-  1. (More practice with variables!) Change the variable name of "lightSensor" to "lightLevel"
-  2. Open the Serial Monitor and watch the data.  Now close this and open the Serial Plotter.  Cool, huh?
-  3. Change the sampling period to 1s, note the difference in sampling rate on the Serial Plotter
-  4. What is the highest light value the sensor can detect? (try using your cell phone flashlight)
-  5. What is the lowest light value the sensor can detect? (cover it with your finger)
+  1. Open the Serial Monitor and watch the data. Now close this and open the Serial Plotter. Cool, huh?
+  2. Change the delay to 1s, note the difference in sampling rate on the Serial Plotter
+  3. What is the highest light value the sensor can detect? (try using your cell phone flashlight)
+  4. What is the lowest light value the sensor can detect? (cover it with your finger)
 
 */
