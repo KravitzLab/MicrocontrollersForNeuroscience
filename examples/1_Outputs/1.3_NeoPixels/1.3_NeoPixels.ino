14160326a76be7394cc36e16fbde62b17b8b3ab7
Kravitz Lab <alexxai@wustl.edu>
2022-07-30 10:50:41 -0400

    updated examples

--- examples/1_Outputs/1.3_NeoPixels/1.3_NeoPixels.ino
+++ examples/1_Outputs/1.3_NeoPixels/1.3_NeoPixels.ino
@@ -1,10 +1,17 @@
 /*
-  Example 1.3: Light a single Neopixel
-
-  We blinked the red LED, and "blinked" the speaker, now let's blink the Neopixels!
+  Example 1.3: Controlling the Neopixels
 
   This code uses the built-in NeoPixels, a multi-color LED that has its own controller chip.
   This allows you to select individual LEDs and change their colors using only one digital pin.
+
+  In this example we will learn to control the neopixels, and see that you can "blink" them just
+  like traditional LEDs.
+
+  pixels.setPixelColor(n, r, g, b) is a function for setting the color of each NeoPixel on the PyGamer.  Useage:
+    n  Pixel index, from 0 to 4 (5 pixels in total)
+    r Red brightness, 0 = minimum (off), 255 = maximum.
+    g Green brightness, 0 = minimum (off), 255 = maximum.
+    b Blue brightness, 0 = minimum (off), 255 = maximum.
 
   This example utilizes libraries and code from Adafruit.
   We encourage users to support open-source hardware and software!
@@ -24,12 +31,6 @@ void setup() {
 }
 
 void loop() {
-  /* pixels.setPixelColor(n, r, g, b) is a function that can be used to set the color of each NeoPixel on the PyGamer.  Useage:
-    n  Pixel index, from 0 to 4 (5 pixels in total)
-    r Red brightness, 0 = minimum (off), 255 = maximum.
-    g Green brightness, 0 = minimum (off), 255 = maximum.
-    b Blue brightness, 0 = minimum (off), 255 = maximum.
-  */
   pixels.setPixelColor(0, 20, 0, 0);                              // Turn left-most pixel on red (Experiment with changing this line! Can you change the color? The pixel?)
   pixels.show();                                                  // Call pixels.show to light up the pixels
 }
@@ -38,8 +39,9 @@ void loop() {
 
   Activities:
   1. Can you change the above example to light up the 2nd pixel?
-  2. Can you change the pixels to blink?
-  3. Can you turn on all of the LEDs at once? Can you find a more efficient way of going this?
-  (hint: View the NeoPixel class reference documentation)
+  2. Change the color of the blinking NeoPixels to purple (red/blue)
+  3. Can you change the pixels to blink? (Hint: You will have to call "pixels.show();" twice to do this
+  4. Can you turn on all of the Neopixels?
+  5. Can you make the Neopixels light up blue in sequence from left to right?
 
 */
