538f1cca8d9f744c1d7324d76fa72e321e5e380e
KCl94 <109825179+KCl94@users.noreply.github.com>
2022-07-31 20:09:32 -0400

    Update 1.2_PlayTone.ino

--- examples/1_Outputs/1.2_PlayTone/1.2_PlayTone.ino
+++ examples/1_Outputs/1.2_PlayTone/1.2_PlayTone.ino
@@ -42,8 +42,6 @@ Activities:
 1. What happens if you #define speaker as A2 but make no other changes to the code?  Make your prediction before trying it!
 2. Can you change the name of "speaker" to "buzzer"? 
 3. Can you make a 1000Hz tone?
-4. Does anything change if you pull the speaker LOW in line 31 and then HIGH in line 33?  Make your prediction before trying this.
-5. (Advanced) Changing the delay changes the tone frequency, how might you change the volume of the tone?  
-   (Hint: Arduino also has an analogWrite function - Google it!)
+4. Does anything change if you pull the speaker LOW in line 33 and then HIGH in line 35?  Make your prediction before trying this.
 
 */
